c2163767bd4fd280655f7e8aa85d2f4f3c6572ea
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-04 20:38:57 +0200

    After Lesson 1 Section 1.3 spring test support

--- src/test/java/com/packtpub/yummy/YummyApplicationTests.java
+++ src/test/java/com/packtpub/yummy/YummyApplicationTests.java
@@ -2,8 +2,15 @@ package com.packtpub.yummy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +20,15 @@ public class YummyApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Autowired
+    DatePrinter datePrinter;
+
+	@MockBean
+    TimeFactory timeFactory;
+
+	@Test
+    public void datePrinterWorks(){
+	    given(timeFactory.now()).willReturn(LocalDateTime.of(2017,5,1,17,41,12));
+	    assertEquals("Now it is 2017-05-01T17:41:12", datePrinter.printDate());
+    }
 }
