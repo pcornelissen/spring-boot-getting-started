07e2bd0aa96491db366b64aae0c904c82f8f66eb
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-12 18:48:48 +0200

    After Lesson 1 Section 3.7 closing section 3, resource assembler and responseStatus

--- src/main/java/com/packtpub/yummy/BookmarkController.java
+++ src/main/java/com/packtpub/yummy/BookmarkController.java
@@ -11,28 +11,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
 @RestController
 @RequestMapping("bookmark")
 public class BookmarkController {
+    @Autowired
+    BookmarkResourceAssembler assembler;
     @Autowired
     BookmarkService bookmarkService;
 
     @GetMapping("{id}")
     public Resource<Bookmark> getBookmark(@PathVariable UUID id){
-        return new Resource<>(bookmarkService.find(id),
-                linkTo(methodOn(BookmarkController.class).getBookmark(id)).withSelfRel()
-                );
-    }
-    @PostMapping("{id}")
-    public Resource<Bookmark> updateBookmark(@PathVariable UUID id, @RequestBody Bookmark bookmark){
-        return new Resource<>(bookmarkService.update(bookmark.withUuid(id)),
-                linkTo(methodOn(BookmarkController.class).getBookmark(id)).withSelfRel()
-                );
+        return assembler.toResource(bookmarkService.find(id));
     }
 
+    @PostMapping("{id}")
+    public Resource<Bookmark> updateBookmark(@PathVariable UUID id, @RequestBody Bookmark bookmark){
+        return assembler.toResource(bookmarkService.update(bookmark.withUuid(id)));
+    }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteBookmark(@PathVariable UUID id){
---
+++ src/main/java/com/packtpub/yummy/BookmarkResourceAssembler.java
@@ -0,0 +1,19 @@
+package com.packtpub.yummy;
+
+import com.packtpub.yummy.model.Bookmark;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Component
+public class BookmarkResourceAssembler implements ResourceAssembler<Bookmark, Resource<Bookmark>> {
+
+    @Override
+    public Resource<Bookmark> toResource(Bookmark entity) {
+        return new Resource<>(entity,
+                linkTo(methodOn(BookmarkController.class).getBookmark(entity.getUuid())).withSelfRel());
+    }
+}
