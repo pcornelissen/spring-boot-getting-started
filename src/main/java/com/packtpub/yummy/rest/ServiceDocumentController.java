d2be08e133eee7b12313a382cb29fc666dd02dad
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-24 20:35:33 +0200

    added some beans+tests to play around with validation

--- src/main/java/com/packtpub/yummy/rest/ServiceDocumentController.java
+++ src/main/java/com/packtpub/yummy/rest/ServiceDocumentController.java
@@ -1,12 +1,18 @@
 package com.packtpub.yummy.rest;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.BasicLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
 import java.net.URISyntaxException;
+import java.util.Date;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -19,6 +25,97 @@ public class ServiceDocumentController {
         return new Resource<>("Yummy is the best service",
                 BasicLinkBuilder.linkToCurrentMapping().withSelfRel(),
                 linkTo(methodOn(BookmarksController.class).addBookmark(null)).withRel("bookmarks")
-                );
+        );
+    }
+
+    @PostMapping("/validate")
+    public Resource<String> validate(@RequestBody @Valid ValidateMe validateMe) {
+        return new Resource<>("OK");
+    }
+
+    public static class ValidateMe {
+        @Min(21)
+        int age;
+        @Length(min = 4, max = 6)
+        String name;
+        @Min(5)
+        @Max(1000)
+        Integer arbitraryValue;
+        ValidateMe child;
+        @Email
+        String email;
+        @Future
+        Date nextBirthday;
+        @Past
+        Date Birthday;
+
+        public ValidateMe(int age, String name, Integer arbitraryValue, ValidateMe child, String email, Date nextBirthday, Date birthday) {
+            this.age = age;
+            this.name = name;
+            this.arbitraryValue = arbitraryValue;
+            this.child = child;
+            this.email = email;
+            this.nextBirthday = nextBirthday;
+            Birthday = birthday;
+        }
+
+        public ValidateMe() {
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getArbitraryValue() {
+            return arbitraryValue;
+        }
+
+        public void setArbitraryValue(Integer arbitraryValue) {
+            this.arbitraryValue = arbitraryValue;
+        }
+
+        public ValidateMe getChild() {
+            return child;
+        }
+
+        public void setChild(ValidateMe child) {
+            this.child = child;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Date getNextBirthday() {
+            return nextBirthday;
+        }
+
+        public void setNextBirthday(Date nextBirthday) {
+            this.nextBirthday = nextBirthday;
+        }
+
+        public Date getBirthday() {
+            return Birthday;
+        }
+
+        public void setBirthday(Date birthday) {
+            Birthday = birthday;
+        }
     }
 }
--- src/test/java/com/packtpub/yummy/rest/ServiceDocumentControllerTest.java
+++ src/test/java/com/packtpub/yummy/rest/ServiceDocumentControllerTest.java
@@ -9,10 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,7 +24,7 @@ import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,18 +39,88 @@ public class ServiceDocumentControllerTest {
     public void getServiceDocument() throws Exception {
         String result = mvc.perform(
                 MockMvcRequestBuilders.get("/")
-                .accept("application/hal+json;charset=UTF-8")
+                        .accept("application/hal+json;charset=UTF-8")
         ).andDo(print())
                 .andExpect(content()
                         .contentTypeCompatibleWith("application/hal+json;charset=UTF-8"))
                 .andReturn().getResponse().getContentAsString();
 
-        Resource<String> value = mapper.readValue(result, new TypeReference<Resource<String>>(){});
+        Resource<String> value = mapper.readValue(result, new TypeReference<Resource<String>>() {
+        });
 
         List<String> linkRels = value.getLinks().stream().map(link -> link.getRel()).collect(Collectors.toList());
         assertThat(linkRels, Matchers.hasItem("self"));
-        assertEquals(value.getLink("self"),value.getId());
+        assertEquals(value.getLink("self"), value.getId());
 
         assertTrue(value.hasLink("bookmarks"));
+    }
+
+    @Test
+    public void validateSucceeds() throws Exception {
+        ServiceDocumentController.ValidateMe vm = getValidValidateMe();
+        mvc.perform(
+                MockMvcRequestBuilders.post("/validate")
+                        .content(new ObjectMapper().writeValueAsString(vm))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept("application/hal+json;charset=UTF-8")
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void validateSucceedsWithInvalidChild() throws Exception {
+        ServiceDocumentController.ValidateMe vm = getValidValidateMe();
+        vm.getChild().setAge(0);
+        mvc.perform(
+                MockMvcRequestBuilders.post("/validate")
+                        .content(new ObjectMapper().writeValueAsString(vm))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept("application/hal+json;charset=UTF-8")
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void validateFailsWithFutureDateOfBirth() throws Exception {
+        ServiceDocumentController.ValidateMe vm = getValidValidateMe();
+        vm.setBirthday(vm.getNextBirthday());
+        mvc.perform(
+                MockMvcRequestBuilders.post("/validate")
+                        .content(new ObjectMapper().writeValueAsString(vm))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept("application/hal+json;charset=UTF-8")
+        ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].field").value("Birthday"));
+    }
+
+    @Test
+    public void validateFailsWithWrongEmail() throws Exception {
+        ServiceDocumentController.ValidateMe vm = getValidValidateMe();
+        vm.setEmail("IAmNoEmail@");
+        mvc.perform(
+                MockMvcRequestBuilders.post("/validate")
+                        .content(new ObjectMapper().writeValueAsString(vm))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept("application/hal+json;charset=UTF-8")
+        ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].field").value("email"));
+    }
+
+    private ServiceDocumentController.ValidateMe getValidValidateMe() {
+        return new ServiceDocumentController.ValidateMe(
+                21,
+                "name",
+                5,
+                new ServiceDocumentController.ValidateMe(
+                        30, "name2", null, null, "email@here.there",
+                        Date.from(LocalDateTime.now().plusMonths(5).toInstant(ZoneOffset.UTC)),
+                        Date.from(LocalDateTime.now().minusHours(5).toInstant(ZoneOffset.UTC))
+                ),
+                "email@from.space",
+                Date.from(LocalDateTime.now().plusMonths(5).toInstant(ZoneOffset.UTC)),
+                Date.from(LocalDateTime.now().minusWeeks(5).toInstant(ZoneOffset.UTC))
+        );
     }
 }
