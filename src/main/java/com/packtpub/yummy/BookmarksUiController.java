7454287e753ec2ca727669b4a223dea7f722a324
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-24 12:08:29 +0200

    After Lesson 1 Section 5.3 adding bookmark list page

--- src/main/java/com/packtpub/yummy/BookmarksUiController.java
+++ src/main/java/com/packtpub/yummy/BookmarksUiController.java
@@ -5,20 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.UUID;
-
 @Controller
-@RequestMapping("/bookmark")
-public class BookmarkUiController {
+@RequestMapping("/")
+public class BookmarksUiController {
     @Autowired
     BookmarkService bookmarkService;
 
-    @GetMapping("{id}")
-    public String details(@PathVariable UUID id, Model model){
-        model.addAttribute("bookmark", bookmarkService.find(id));
-        return "bookmark/details";
+    @GetMapping
+    public String listOfBookmarks(Model model){
+        model.addAttribute("bookmarks", bookmarkService.findAll());
+        return "bookmarks/list";
     }
 }
