b02a557c05baf85b443888e350f307352ca9596b
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-24 14:36:25 +0200

    After Lesson 1 Section 5.4 bookmark delete form

--- src/main/java/com/packtpub/yummy/BookmarkUiController.java
+++ src/main/java/com/packtpub/yummy/BookmarkUiController.java
@@ -6,19 +6,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/bookmark")
+@RequestMapping("/bookmark/{id}")
 public class BookmarkUiController {
     @Autowired
     BookmarkService bookmarkService;
 
-    @GetMapping("{id}")
+    @GetMapping
     public String details(@PathVariable UUID id, Model model){
         model.addAttribute("bookmark", bookmarkService.find(id));
         return "bookmark/details";
+    }
+
+    @PostMapping("delete")
+    public String delete(@PathVariable UUID id){
+        bookmarkService.delete(id);
+        return "redirect:/";
     }
 }
