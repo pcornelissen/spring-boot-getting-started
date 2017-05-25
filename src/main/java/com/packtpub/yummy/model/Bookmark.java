6e8356b201dbb181bfe55e86d7bf24495a31981f
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-25 21:35:23 +0200

    After Lesson 1 Section 6.4 added validation to the html ui

--- src/main/java/com/packtpub/yummy/model/Bookmark.java
+++ src/main/java/com/packtpub/yummy/model/Bookmark.java
@@ -3,20 +3,20 @@ package com.packtpub.yummy.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.URL;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Bookmark {
     @Length(max=255)
-    @NotNull
+    @NotEmpty
     private String description;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
     @Length(max=255)
-    @NotNull
+    @NotEmpty
     @URL
     private String url;
     private UUID uuid;
@@ -94,5 +94,13 @@ public class Bookmark {
 
     public LocalDateTime getUpdatedOn() {
         return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
     }
 }
--- src/main/java/com/packtpub/yummy/ui/BookmarkUiController.java
+++ src/main/java/com/packtpub/yummy/ui/BookmarkUiController.java
@@ -5,11 +5,13 @@ import com.packtpub.yummy.service.BookmarkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Controller
@@ -39,14 +41,26 @@ public class BookmarkUiController {
     }
 
     @PostMapping("{id}")
-    public String saveBookmark(@PathVariable UUID id, Bookmark bookmark) {
+    public String saveBookmark(@PathVariable UUID id, @Valid Bookmark bookmark,
+                               BindingResult bindingResult, Model model
+    ) {
         bookmark.setUuid(id);
+        if (bindingResult.hasErrors()){
+            model.addAttribute("isEdit", true);
+            return "bookmark/edit";
+        }
         bookmarkService.update(bookmark);
         return "redirect:/";
     }
 
     @PostMapping
-    public String addBookmark(Bookmark bookmark) {
+    public String addBookmark(@Valid Bookmark bookmark,
+                              BindingResult bindingResult, Model model
+    ) {
+        if (bindingResult.hasErrors()){
+            model.addAttribute("isEdit", false);
+            return "bookmark/edit";
+        }
         bookmarkService.addBookmark(bookmark);
         return "redirect:/";
     }
