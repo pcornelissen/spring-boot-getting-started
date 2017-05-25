73cbe589b57c38399e877c8212f460862d51688f
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-05-25 17:21:11 +0200

    After Lesson 1 Section 6.3.1 added the "add form" + minor beautifications and refactorings

--- src/main/java/com/packtpub/yummy/ui/BookmarkUiController.java
+++ src/main/java/com/packtpub/yummy/ui/BookmarkUiController.java
@@ -5,31 +5,53 @@ import com.packtpub.yummy.service.BookmarkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/bookmark/{id}")
+@RequestMapping("/bookmark/")
 public class BookmarkUiController {
     @Autowired
     BookmarkService bookmarkService;
 
-    @GetMapping
-    public String details(@PathVariable UUID id, @RequestParam(value = "edit", defaultValue = "false") boolean editMode,
-                          Model model) {
+    @GetMapping("{id}")
+    public String details(@PathVariable UUID id, Model model) {
         model.addAttribute("bookmark", bookmarkService.find(id));
-        return editMode ? "bookmark/edit" : "bookmark/details";
+        return "bookmark/details";
     }
 
-    @PostMapping
-    public String saveBookmark(@PathVariable UUID uuid, Bookmark bookmark) {
-        bookmark.setUuid(uuid);
+    @GetMapping(value = "{id}", params = "edit=true")
+    public String editForm(@PathVariable UUID id, Model model) {
+        model.addAttribute("bookmark", bookmarkService.find(id));
+        model.addAttribute("isEdit", true);
+        return "bookmark/edit";
+    }
+
+    @GetMapping()
+    public String addForm(Model model) {
+        model.addAttribute("bookmark", new Bookmark());
+        model.addAttribute("isEdit", false);
+        return "bookmark/edit";
+    }
+
+    @PostMapping("{id}")
+    public String saveBookmark(@PathVariable UUID id, Bookmark bookmark) {
+        bookmark.setUuid(id);
         bookmarkService.update(bookmark);
         return "redirect:/";
     }
 
-    @PostMapping("delete")
+    @PostMapping
+    public String addBookmark(Bookmark bookmark) {
+        bookmarkService.addBookmark(bookmark);
+        return "redirect:/";
+    }
+
+    @PostMapping("{id}/delete")
     public String delete(@PathVariable UUID id) {
         bookmarkService.delete(id);
         return "redirect:/";
